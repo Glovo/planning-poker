d9bc49e4d264e767c3e5eb5bac3dc2eb66551928
Michal Fiuk <majkel785@gmail.com>
2020-06-14 14:19:42 +0200

    Test rework: SpecSelectedTest for more readable

--- src/test/java/com/glovoapp/planningpoker/e2e/spectatorbox/SpecSelectedTest.java
+++ src/test/java/com/glovoapp/planningpoker/e2e/spectatorbox/SpecSelectedTest.java
@@ -15,6 +15,9 @@ import static com.glovoapp.planningpoker.e2e.WebDriverFactory.getDriver;
 class SpecSelectedTest {
 
     private final WebDriver driver = getDriver();
+    private MainPage mainPage = new MainPage(driver);
+    private SessionPage sessionPage = new SessionPage(driver);
+    private final String userName = "username";
 
     @AfterEach
     void releaseDriver() {
@@ -22,29 +25,24 @@ class SpecSelectedTest {
     }
 
     @Test
-    void shouldWork() {
-        // Przejdź do strony
-        MainPage browserM = new MainPage(driver);
-        browserM.openGlovoSite();
-        //znajdz Create random session i dolacz
-        browserM.joinRandomSession();
-        //Znajdz Your name i wprowadz userName
-        SessionPage browserP = new SessionPage(driver);
-        //User1 amd User2 name to enter
-        String userName = "michal";
-        browserP.enterName(userName);
-        //znajdz Selector checkbox i go kliknij
-        browserP.specClick();
+    void whenAUserWithTheNameSelectsSpectator_HisVoteShouldNotAppear() {
+
+        mainPage.openGlovoSite();
+        mainPage.joinRandomSession();
+        sessionPage.enterName(userName);
+        sessionPage.specClick();
+
+        //A break is required so that the website can update
         try {
             Thread.sleep(1_000);
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
-        //przycisnij 2
-        Optional<WebElement> przycisk2 = browserP.getVotingButton("2");
-        przycisk2.get().click();
 
-        browserP.emptyTableCheck();
+        Optional<WebElement> button2 = sessionPage.getVotingButton("2");
+        button2.get().click();
+        //Checking if the scoreboard is empty
+        sessionPage.emptyTableCheck();
 
 
     }
